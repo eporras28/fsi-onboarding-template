6855101fe3f272be81713cb0fe22934c87873282
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-11-27 17:59:53 +0100

    Wait for progress now simply runs as container user.

--- openshift/postgresql/contrib/wait_for_postgres.sh
+++ openshift/postgresql/contrib/wait_for_postgres.sh
@@ -5,7 +5,8 @@ set -e
 cmd="$@"
 timer="5"
 
-until runuser -l postgres -c 'pg_isready' 2>/dev/null; do
+#until runuser -l postgres -c 'pg_isready' 2>/dev/null; do
+pg_isready 2>/dev/null; do
   >&2 echo "Postgres is unavailable - sleeping for $timer seconds"
   sleep $timer
 done
