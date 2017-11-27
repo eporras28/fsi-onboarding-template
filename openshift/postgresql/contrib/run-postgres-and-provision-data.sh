20d8ff2725233c124a7843bc42deeb3b4e6944d7
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-11-27 18:21:56 +0100

    PostgreSQL DB is now automatically provisioned on boot.

---
+++ openshift/postgresql/contrib/run-postgres-and-provision-data.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+/var/lib/pgsql/wait_for_postgres.sh /var/lib/pgsql/provision_data.sh & run-postgresql
+
--- openshift/postgresql/contrib/wait_for_postgres.sh
+++ openshift/postgresql/contrib/wait_for_postgres.sh
@@ -6,7 +6,7 @@ cmd="$@"
 timer="5"
 
 #until runuser -l postgres -c 'pg_isready' 2>/dev/null; do
-pg_isready 2>/dev/null; do
+until pg_isready 2>/dev/null; do
   >&2 echo "Postgres is unavailable - sleeping for $timer seconds"
   sleep $timer
 done
